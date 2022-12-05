ad936d6496f69d6fc5e2b65e725cf329a6cb3105
Rick Visser <119862891+rick-visser@users.noreply.github.com>
2022-12-05 16:57:33 +0100

    Changed the navbar to a sidenav

---
+++ resources/views/components/sidenav/dropdown-item.blade.php
@@ -0,0 +1,18 @@
+@props([
+    'route' => false,
+    'svg' => false,
+    'title' => false,
+    'active' => false,
+])
+
+@php
+    $classes = ($active ?? false)
+                ? 'text-gray-100 bg-green-600 text-gray-900 group flex items-center px-2 py-2 text-md font-bold rounded-md'
+                : 'text-gray-200 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-md font-semibold rounded-md';
+@endphp
+
+<a {{ $attributes->merge(['class' => $classes]) }} x-state-description="undefined: &quot;bg-gray-100 text-gray-900&quot;, undefined: &quot;text-gray-600 hover:bg-gray-50 hover:text-gray-900&quot;">
+    -{{ $slot }}
+</a>
+
+{{----}}
---
+++ resources/views/livewire/tools/time.blade.php
@@ -0,0 +1,13 @@
+<div class="text-center mx-auto" wire:poll.1000ms>
+    <h1 class="text-md capitalize font-semibold">
+   @php
+       setlocale(LC_TIME, 'NL_nl');
+       echo strftime('%A %E %e %B %Y',time());
+   @endphp
+        <br>
+   @php
+       setlocale(LC_TIME, 'NL_nl');
+       echo strftime('%H:%M:%S',time());
+   @endphp
+    </h1>
+</div>
