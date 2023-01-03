3db12b21ce3ace2bac2d721ddce4c8493a1ad1ab
Rick Visser <rickvisser99@gmail.com>
2023-01-03 15:09:07 +0100

    Version 2.0: updated routes

--- routes/web.php
+++ routes/web.php
@@ -6,27 +6,15 @@ use App\Http\Controllers\PagesController;
 
 Route::controller(PagesController::class)->group( function (){
     Route::get('/', 'renderHome')->name('home');
-    Route::get('contact', 'renderContact')->name('contact');
-    Route::get('contact/send-mail', 'sendmail')->name('contact.send-mail');
-    Route::prefix('experience')->group( function () {
+    Route::get('bericht-mij', 'renderContact')->name('contact');
+    Route::get('bericht-mij/send-mail', 'sendmail')->name('contact.send-mail');
+    Route::prefix('ervaring')->group( function () {
         Route::get('/', 'renderExperienceIndex')->name('experience');
-        Route::prefix('schools')->group( function () {
-            Route::get('/{schoolId}/{page}', 'renderSchoolShow')->name('schools.show');
-        });
-        Route::prefix('jobs')->group( function () {
-            Route::get('/{jobId}/{page}', 'renderJobShow')->name('jobs.show');
-        });
-        Route::prefix('internships')->group( function () {
-            Route::get('/{internshipId}/{page}', 'renderInternshipShow')->name('internships.show');
-        });
-//        Route::prefix('certificates')->group( function () {
-//            Route::get('/{certificateId}/{page}', 'renderCertificateShow')->name('certificates.show');
-//        });
     });
-    Route::prefix('about-me')->group( function () {
+    Route::prefix('over-mij')->group( function () {
         Route::get('/', 'renderAboutIndex')->name('about');
     });
-    Route::prefix('projects')->group( function () {
+    Route::prefix('projecten')->group( function () {
         Route::get('/', 'renderProjectsIndex')->name('projects');
     });
 });
